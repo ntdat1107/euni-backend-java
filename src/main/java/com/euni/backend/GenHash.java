2def0863f0992b8324ee95cafb2993b2a59e076f
datnt2 <datnt2@aequitas.com.vn>
2026-05-10 21:46:27 +0700

    Config docker

--- src/main/java/com/euni/backend/GenHash.java
+++
@@ -1,9 +0,0 @@
-package com.euni.backend;
-
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-public class GenHash {
-    public static void main(String[] args) {
-        System.out.println(new BCryptPasswordEncoder().encode("eUni123456"));
-    }
-}
